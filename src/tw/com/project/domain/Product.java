b119d044e7f501e797083210633b21fcfc3a2e6b
Wu Ray <58294174+s1063724@users.noreply.github.com>
2024-05-21 02:41:19 +0800

    Add files via upload

---
+++ src/tw/com/project/domain/Product.java
@@ -0,0 +1,51 @@
+package tw.com.project.domain;
+
+public class Product {
+	private int id;
+	private String name;
+	private int price;
+	private String intro;
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", price=" + price + ", intro=" + intro + "]";
+	}
+	
+}
